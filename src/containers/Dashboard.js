777203ef242facf2c1e20c6e7d1583126127450d
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2019-12-02 00:55:42 -0600

    getting errors

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,11 +1,18 @@
 import { connect } from 'react-redux'
 import Dashboard from '../components/Dashboard'
+import { removeCar } from '../redux/actions'
 // import { removeCar } action here
 
 const mapStateToProps = (state) => {
     return {
         user: state.user,
         cars: state.cars
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        removeCar: (index) => dispatch(removeCar(index))
     }
 }
 
